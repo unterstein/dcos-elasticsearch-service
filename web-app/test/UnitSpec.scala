1b94631717cff2ad0e3ab2f6a96863a5541958a4
Johannes Unterstein <junterstein@mesosphere.io>
2017-11-01 11:05:54 +0100

    added crsf token for testing

--- web-app/test/UnitSpec.scala
+++ web-app/test/UnitSpec.scala
@@ -1,19 +1,19 @@
-import akka.actor.ActorSystem
 import controllers.AuthorizationController
 import org.scalatestplus.play._
-import play.api.test.Helpers._
 import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.test.CSRFTokenHelper._
 
 /**
- * Unit tests can run without a full Play application.
- */
+  * Unit tests can run without a full Play application.
+  */
 class UnitSpec extends PlaySpec {
 
   "ApplicationController" should {
 
     "return a valid result with action" in {
       val controller = new AuthorizationController(stubControllerComponents())
-      val result = controller.loginPage(FakeRequest())
+      val result = controller.loginPage(FakeRequest().withCSRFToken)
       contentAsString(result) must include("Elasticsearch as a service web app")
     }
   }

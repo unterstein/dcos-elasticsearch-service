58622a1568957ec39d6dd1c24e98415df6b05cf9
Johannes Unterstein <junterstein@mesosphere.io>
2017-11-12 18:48:33 +0100

    changed deployment group

--- web-app/test/DeploymentSpec.scala
+++ web-app/test/DeploymentSpec.scala
@@ -14,7 +14,7 @@ class DeploymentSpec extends PlaySpec {
       val user = User("asd@asd.de", "kjadskjasdlsadh")
       val app = App("adslkadsklasdkjl", "testapp", 1, 64, 1000, 9200)
       val deploymentDescriptor = DeploymentHelper.rendeTemplate(user, app).replace(" ", "").replace("\\n", "")
-      deploymentDescriptor must include("\"id\":\"/userelasticsearch/adslkadsklasdkjl\"")
+      deploymentDescriptor must include("\"id\":\"/eaas/es/adslkadsklasdkjl\"")
       deploymentDescriptor must include("\"cpus\":1.0")
       deploymentDescriptor must include("\"mem\":64.0")
       deploymentDescriptor must include("\"VIP_0\":\"adslkadsklasdkjl:9200\"")
